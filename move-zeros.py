ea285f84afb87b4657f85b03956cde45e9c960f0
Ayush Kumar <ayushkumar47834@gmail.com>
2026-06-09 16:25:28 +0530

    Add moveZeroes method to move zeros in list
    Implement moveZeroes method to shift zeros in-place.

---
+++ move-zeros.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/move-zeroes/?envType=study-plan-v2&envId=programming-skills
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                temp = nums[i]
+                nums[i] = nums[count]
+                nums[count] = temp
+                count += 1
+    
+        return nums
+        
