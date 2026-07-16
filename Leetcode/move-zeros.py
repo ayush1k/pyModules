c9c640656c66196ed6d5177b742dc1bd68b8cb85
Ayush Kumar <ayushkumar47834@gmail.com>
2026-07-16 17:07:17 +0530

    Implement in-place zero movement algorithm
    Added a new approach to move zeros in-place.

--- Leetcode/move-zeros.py
+++ Leetcode/move-zeros.py
@@ -16,3 +16,18 @@ class Solution(object):
     
         return nums
         
+# approach 2 4ms
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[count] = nums[count], nums[i]
+                count += 1
+    
+        # return nums
+        
