b7f0cf7d55c2dd3976f60878f92dc140350881f0
Ayush Kumar <ayushkumar47834@gmail.com>
2026-07-16 17:07:56 +0530

    Add second approach for repeated substring pattern

--- Leetcode/RepeatedSubString.py
+++ Leetcode/RepeatedSubString.py
@@ -13,3 +13,20 @@ class Solution(object):
             if chunk * multiplier == s:
                 return True
         return False
+
+# approach 2
+class Solution(object):
+    def repeatedSubstringPattern(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        n =  len(s)
+        for i in range(1, n//2 + 1):
+            if n%i == 0:
+                chunks = s[: i]
+                multiplier = n//i
+
+            if (chunks * multiplier) == s:
+                return True
+        return False
